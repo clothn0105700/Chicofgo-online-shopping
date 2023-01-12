17239d7d2373cddee65cb6defe66fdcaf04c1bb2
clothn0105700 <clothn0105700@gmail.com>
2023-01-12 15:18:45 +0800

    [feat]App.js

--- Chicofgo_react_main/src/App.js
+++ Chicofgo_react_main/src/App.js
@@ -1,11 +1,17 @@
-// import Navbar from './component/navbar'
-import Navbaro from './component/navbar-or'
+import Navbar from './component/navbar/navbar'
+import Member from './routes/member/member'
+import ChangePassword from './component/member/changePassword'
+import Sidebar from './component/sidebar/sidebar'
+// import Navbaro from './component/navbar-or'
 // import Carousel from './component/carousel'
 
 function App() {
   return (
     <>
-      <Navbaro />
+      <Navbar />
+      <Member />
+      <ChangePassword />
+      {/* <Sidebar /> */}
     </>
   )
 }
