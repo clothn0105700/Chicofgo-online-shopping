c3f51de4e667565d077a2d374477fa56cd567193
cc79997 <gary860608@gmail.com>
2023-02-06 15:56:17 +0800

    新增活動專區跳頁功能

--- Chicofgo_react_main/src/Pages/Event/Event.js
+++ Chicofgo_react_main/src/Pages/Event/Event.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Carousels from './Component/Carousels';
+import Carousels from './Component/EventCarousels';
 import style from './Event.module.scss';
 
 function Event() {
   return (
     <Container fluid>
-      <Row>
-        <Col xs={12} className={`${style.event}`}>
-          <Carousels
-            showImg={[
-              '111.jpg',
-              'Carousels-1.png',
-              'Carousels-2.png',
-              'Carousels-3.png',
-            ]}
-            changeDelay={2000}
-            fade={true}
-          />
-        </Col>
-        <Col xs={12}>
-          <Carousels
-            showImg={['Carousels-1.png', 'Carousels-2.png', 'Carousels-3.png']}
-            changeDelay={2000}
-            fade={true}
-          />
-        </Col>
-        <Col xs={12}>
-          <Carousels
-            showImg={['Carousels-1.png', 'Carousels-2.png', 'Carousels-3.png']}
-            changeDelay={2000}
-            fade={true}
-          />
-        </Col>
-      </Row>
+      <Carousels
+        className={`${style.event}`}
+        showImg={['Carousels-1.png', 'Carousels-2.png', 'Carousels-3.png']}
+        srcollTo={0}
+        changeDelay={2000}
+        fade={true}
+      />
+      <Carousels
+        showImg={['Carousels-2.png', 'Carousels-3.png', 'Carousels-1.png']}
+        srcollTo={window.innerHeight}
+        changeDelay={2000}
+        fade={true}
+      />
+      <Carousels
+        showImg={['Carousels-3.png', 'Carousels-1.png', 'Carousels-2.png']}
+        srcollTo={2 * window.innerHeight}
+        changeDelay={2000}
+        fade={true}
+      />
+      <Carousels
+        showImg={['Carousels-1.png', 'Carousels-2.png', 'Carousels-3.png']}
+        srcollTo={3 * window.innerHeight}
+        changeDelay={2000}
+        fade={true}
+      />
     </Container>
   );
 }
